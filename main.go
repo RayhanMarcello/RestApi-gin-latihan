5f9da509cd5a7d13c96a65ff2ef527850f71eac6
Rayhan Marcello <rayhanmarcello222@gmail.com>
2026-01-06 00:54:53 +0800

    menambahkan root delete

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 	v1.GET("/", handler.GetHandler)
 	v1.POST("/post", handler.PostHandler)
 	v1.PATCH("/:id", handler.UpdateHandler)
+	v1.DELETE("/:id", handler.DeleteHandler)
 
 	router.Run(":8000")
 }
