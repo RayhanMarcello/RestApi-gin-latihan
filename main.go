ddce6ef205a58bf6c4f9f9720c7192d65ba1911a
Rayhan Marcello <rayhanmarcello222@gmail.com>
2026-01-06 00:35:33 +0800

    fix handler and use databases

--- main.go
+++ main.go
@@ -10,19 +10,16 @@ import (
 func main() {
 	router := gin.Default()
 
-
 	// db connection
 	database.Database()
+
 	//versioning
 	v1 := router.Group("/v1")
 
 	// path
-	v1.GET("/", handler.RootHandler)
+	v1.GET("/", handler.GetHandler)
 	v1.POST("/post", handler.PostHandler)
-
-
-
-
+	v1.PATCH("/:id", handler.UpdateHandler)
 
 	router.Run(":8000")
 }
