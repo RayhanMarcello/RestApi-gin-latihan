49bb33abd9e6bb7024cb223852f8f3c646f93470
Rayhan Marcello <rayhanmarcello222@gmail.com>
2026-01-15 23:13:12 +0800

    refactor db

--- database/db.go
+++ database/db.go
@@ -2,8 +2,6 @@ package database
 
 import (
 	"fmt"
-	"latihan-api-gorm/entity"
-	"latihan-api-gorm/repository"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,7 +9,7 @@ import (
 
 var DB *gorm.DB
 
-func Database() {
+func Database() *gorm.DB{
 	dsn := "app:root@tcp(127.0.0.1:3306)/product?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -19,29 +17,6 @@ func Database() {
 		fmt.Println("error connect to db")
 	}
 	fmt.Println("sucsess connect to db")
-	DB = db
-
-	repositoryLayer := repository.NewRepository(db)
-	product, err := repositoryLayer.FindAll()
-	if err != nil{
-		fmt.Println("cannot find all prod")
-	}
-	// for i := 0; i < len(product); i++{
-	// 	fmt.Println("produk : ", product[i].Name)
-	// }  
-	for i, p := range product {
-    fmt.Println("produk", i, p.Name)
-}
-
-	prod := entity.Product{
-		Name: "kontolodon",
-		Price: 20000,
-	}
-	prods, err := repositoryLayer.Create(prod)
-	fmt.Println(prods)
-
-	id := 1
-	productById, err := repositoryLayer.FindByID(id)
-	fmt.Println("product by id ", id, productById.Name )
-
+	DB = db	
+	return db
 }
